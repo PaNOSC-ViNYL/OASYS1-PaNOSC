a5bafb08ca5d2fae010596a8c5d0f8d883318a79
Aljosa Hafner <aljosah@protonmail.com>
2021-05-31 12:48:20 +0200

    json ingest

--- orangecontrib/panosc/shadow/widgets/extension/ow_remote_beamline_open.py
+++ orangecontrib/panosc/shadow/widgets/extension/ow_remote_beamline_open.py
@@ -1,15 +1,21 @@
 import os, numpy
 
 from PyQt5 import QtGui, QtWidgets
+from PyQt5.QtWidgets import QDialog, QInputDialog
 from orangewidget import gui,widget
 from orangewidget.settings import Setting
 from oasys.widgets import gui as oasysgui, congruence
 from oasys.widgets import widget as oasyswidget
 
+import json
+import urllib.request
+
 from orangecontrib.shadow.util.shadow_objects import ShadowBeam, ShadowOpticalElement, ShadowOEHistoryItem
 
 from orangecontrib.panosc.shadow.util.openPMD import loadShadowOpenPMD
-import Shadow
+from orangecanvas.preview import previewdialog, previewmodel
+
+import six
 
 class RemoteBeamlineLoader(oasyswidget.OWWidget):
     name = "Remote Repository Beamline Loader"
@@ -33,30 +39,80 @@ class RemoteBeamlineLoader(oasyswidget.OWWidget):
     def __init__(self):
         super().__init__()
 
-        self.runaction = widget.OWAction("Read Shadow File", self)
-        self.runaction.triggered.connect(self.read_file)
-        self.addAction(self.runaction)
+        # self.runaction = widget.OWAction("Read Shadow File", self)
+        # self.runaction.triggered.connect(self.read_file)
+        # self.addAction(self.runaction)
 
-        self.setFixedWidth(590)
-        self.setFixedHeight(150)
+        # self.setFixedWidth(590)
+        # self.setFixedHeight(150)
 
-        left_box_1 = oasysgui.widgetBox(self.controlArea, "Shadow File Selection", addSpace=True, orientation="vertical",
-                                         width=570, height=70)
+        """Browse recent schemes. Return QDialog.Rejected if the user
+        canceled the operation and QDialog.Accepted otherwise.
 
-        figure_box = oasysgui.widgetBox(left_box_1, "", addSpace=True, orientation="horizontal", width=550, height=35)
+        """
 
-        self.le_beam_file_name = oasysgui.lineEdit(figure_box, self, "beam_file_name", "Shadow File Name",
-                                                    labelWidth=120, valueType=str, orientation="horizontal")
-        self.le_beam_file_name.setFixedWidth(330)
+        ## https://raw.githubusercontent.com/PaNOSC-ViNYL/Oasys-PaNOSC-Workspaces/master/mainList.json
+        # https://github.com/PaNOSC-ViNYL/Oasys-PaNOSC-Workspaces
 
-        gui.button(figure_box, self, "...", callback=self.selectFile)
+        urlJson = "https://raw.githubusercontent.com/PaNOSC-ViNYL/Oasys-PaNOSC-Workspaces/master/mainList.json"
+        response = urllib.request.urlopen(urlJson)
+        beamlineJson = json.loads(response.read())
 
-        #gui.separator(left_box_1, height=20)
+        
 
-        button = gui.button(self.controlArea, self, "Read Shadow File", callback=self.read_file)
-        button.setFixedHeight(45)
+        self.remote_schemes = []
+        items = [previewmodel.PreviewItem(name=title, path=path)
+                 for title, path in self.recent_schemes]
+        model = previewmodel.PreviewModel(items=items)
 
-        gui.rubber(self.controlArea)
+        dialog = previewdialog.PreviewDialog(self)
+        title = self.tr("Recent Workflows")
+        dialog.setWindowTitle(title)
+        template = ('<h3 style="font-size: 26px">\n'
+                    #'<img height="26" src="canvas_icons:Recent.svg">\n'
+                    '{0}\n'
+                    '</h3>')
+        dialog.setHeading(template.format(title))
+        dialog.setModel(model)
+
+        model.delayedScanUpdate()
+
+        status = dialog.exec_()
+
+        index = dialog.currentIndex()
+
+        dialog.deleteLater()
+        model.deleteLater()
+
+        if status == QDialog.Accepted:
+            doc = self.current_document()
+            if doc.isModifiedStrict():
+                if self.ask_save_changes() == QDialog.Rejected:
+                    return QDialog.Rejected
+
+            selected = model.item(index)
+
+            return self.load_scheme(six.text_type(selected.path()))
+
+        # return status
+
+        # left_box_1 = oasysgui.widgetBox(self.controlArea, "Shadow File Selection", addSpace=True, orientation="vertical",
+        #                                  width=570, height=70)
+        #
+        # figure_box = oasysgui.widgetBox(left_box_1, "", addSpace=True, orientation="horizontal", width=550, height=35)
+        #
+        # self.le_beam_file_name = oasysgui.lineEdit(figure_box, self, "beam_file_name", "Shadow File Name",
+        #                                             labelWidth=120, valueType=str, orientation="horizontal")
+        # self.le_beam_file_name.setFixedWidth(330)
+        #
+        # gui.button(figure_box, self, "...", callback=self.selectFile)
+        #
+        # #gui.separator(left_box_1, height=20)
+        #
+        # button = gui.button(self.controlArea, self, "Read Shadow File", callback=self.read_file)
+        # button.setFixedHeight(45)
+        #
+        # gui.rubber(self.controlArea)
 
     def selectFile(self):
         self.le_beam_file_name.setText(oasysgui.selectFileFromDialog(self, self.beam_file_name, "Open Shadow File"))
@@ -175,9 +231,9 @@ if __name__ == "__main__":
     from PyQt5.QtWidgets import QApplication
 
     a = QApplication(sys.argv)
-    ow = BeamOpenPMDFileReader()
-    ow.le_beam_file_name.setText("/users/srio/Oasys/tmp.h5")
-    ow.workspace_units_to_cm = 100
+    ow = RemoteBeamlineLoader()
+    # ow.le_beam_file_name.setText("/users/srio/Oasys/tmp.h5")
+    # ow.workspace_units_to_cm = 100
     ow.show()
     a.exec_()
     ow.saveSettings()
